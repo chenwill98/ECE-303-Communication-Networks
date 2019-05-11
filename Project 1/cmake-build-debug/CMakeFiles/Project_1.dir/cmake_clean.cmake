1f794043e829e7071c673fbef2113574d767e0b1
chenwill98 <chenwill98@gmail.com>
2019-05-11 17:52:30 -0400

    Initialized repository

---
+++ Project 1/cmake-build-debug/CMakeFiles/Project_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_1.dir/main.cpp.o"
+  "Project_1.pdb"
+  "Project_1.exe"
+  "libProject_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
