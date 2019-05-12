8e678e96362bfd3081acf6a0ede8eff1a452d4b0
chenwill98 <chenwill98@gmail.com>
2019-05-12 10:23:30 -0400

    Files in progress

--- Project 2/receiver.py
+++ Project 2/receiver.py
@@ -1,11 +1,16 @@
 # Written by S. Mevawala, modified by D. Gitzel
 
+# Credit to Leart for helping me
+
 import logging
 
 import channelsimulator
 import utils
 import sys
 import socket
+
+MAX_SEQUENCE = 200
+
 
 class Receiver(object):
 
@@ -23,25 +28,137 @@ class Receiver(object):
         raise NotImplementedError("The base API class has no implementation. Please override and add your own.")
 
 
-class BogoReceiver(Receiver):
-    ACK_DATA = bytes(123)
+# class BogoReceiver(Receiver):
+#     ACK_DATA = bytes(123)
+#
+#     def __init__(self):
+#         super(BogoReceiver, self).__init__()
+#
+#     def receive(self):
+#         self.logger.info(
+#             "Receiving on port: {} and replying with ACK on port: {}".format(self.inbound_port, self.outbound_port))
+#         while True:
+#             try:
+#                 data = self.simulator.u_receive()  # receive data
+#                 self.logger.info("Got data from socket: {}".format(
+#                     data.decode('ascii')))  # note that ASCII will only decode bytes in the range 0-127
+#                 sys.stdout.write(data)
+#                 self.simulator.u_send(BogoReceiver.ACK_DATA)  # send ACK
+#             except socket.timeout:
+#                 sys.exit()
 
-    def __init__(self):
-        super(BogoReceiver, self).__init__()
+
+class ReliableReceiver(Receiver):
+
+    def __init__(self, timeout):
+        super(ReliableReceiver, self).__init__()
+        self.timeout = timeout
+        self.simulator.rcvr_socket.settimeout(self.timeout)
+        self.data_packet = bytearray([0, 0, 0])
+        self.current_ack = bytearray([0, 0])
+        self.ack_resend = 0
+        self.final_ack = -1
+        self.seq = 0
 
     def receive(self):
-        self.logger.info("Receiving on port: {} and replying with ACK on port: {}".format(self.inbound_port, self.outbound_port))
+        self.logger.info(
+            "Receiving on port: {} and replying with ACK on port: {}".format(self.inbound_port, self.outbound_port))
+
         while True:
             try:
-                 data = self.simulator.u_receive()  # receive data
-                 self.logger.info("Got data from socket: {}".format(
-                     data.decode('ascii')))  # note that ASCII will only decode bytes in the range 0-127
-	         sys.stdout.write(data)
-                 self.simulator.u_send(BogoReceiver.ACK_DATA)  # send ACK
+                data_packet = self.simulator.u_receive()  # receive data
+                checksum = data_packet[0]
+                seq_num = data_packet[1]
+                data = data_packet[2:]
+
+                # Compares the received checksum to the generated checksum
+                if checksum == self._checksum(data):
+                    self.seq = (seq_num + 1) % MAX_SEQUENCE
+                    if self.final_ack == -1 or seq_num == self.final_ack:
+                        sys.stdout.write("{}".format(data))
+                        sys.stdout.flush()
+                    self.final_ack = self.seq
+                if self.seq < 0:
+                    self.seq = 0
+                self.current_ack = bytearray([self.seq, self.seq])
+                self._send_ack(self.seq)
+
+                # ACK_seg = Packet()
+                # ACK_success = ACK_seg.ack(data_packet, self.final_ack)
+                # if ACK_success:
+                #     self.final_ack = ACK_seg.ack_num
+                # if ACK_seg.ack_num < 0:
+                #     ACK_seg.ack_num = 0  # we set it to 0 here, it may be set back to -1
+                # # ACK_seg.check_sum = ACK_seg.checksum()
+                # rcv_array = bytearray([ACK_seg.check_sum, ACK_seg.ack_num])
+                # # self.ACK_backup = rcv_array
+                # self.simulator.u_send(rcv_array)
+
+            # If timeout, respond the same way as a corrupted packet
             except socket.timeout:
+                self._error_resend(self.current_ack)
+
+    def _error_resend(self, data_pkt):
+        self.simulator.u_send(data_pkt)
+        self.ack_resend += 1
+        if self.ack_resend >= 3:
+            self.timeout *= 2
+            self.simulator.rcvr_socket.settimeout(self.timeout)
+            self.ack_resend = 0
+            # If there's too many timeouts, it quits
+            if self.timeout >= 6:
                 sys.exit()
+
+    def _send_ack(self, seq_num):
+        # Generates the ACK packet and sends the payload in the correct format
+        ack = Packet(ack_num=seq_num)
+        ack_pkt = bytearray([ack.check_sum, ack.ack_num])
+        self.simulator.u_send(ack_pkt)
+
+    @staticmethod
+    def _checksum(data):
+        check_sum = 0
+        for bit in data:
+            check_sum ^= bit
+        return check_sum
+
+
+# Packet format: [Checksum, ACK]
+class Packet(object):
+
+    def __init__(self, ack_num=0):
+        self.check_sum = self._checksum(ack_num)
+        self.ack_num = ack_num
+
+    # The checksum can just be itself, because what are the odds they get corrupted to the same thing?
+    @staticmethod
+    def _checksum(ack_num):
+        return ack_num
+
+    @staticmethod
+    def _checksum1(data):
+        check_sum = 0
+        for bit in data:
+            check_sum ^= bit
+        return check_sum
+
+    # Checks if the ACK is valid
+    def ack(self, data, last_ACK_num):
+        if self.check_sum == self._checksum1(data):
+            self.ack_num = (data[1] + 1) % 200
+            if data[1] == last_ACK_num or last_ACK_num == -1:
+                sys.stdout.write("{}".format(data[2:]))
+                sys.stdout.flush()
+                return True
+        else:
+            pass
+        return False
+
 
 if __name__ == "__main__":
     # test out BogoReceiver
-    rcvr = BogoReceiver()
+    # rcvr = BogoReceiver()
+    # rcvr.receive()
+
+    rcvr = ReliableReceiver(0.1)
     rcvr.receive()
--- Project 2/sender.py
+++ Project 2/sender.py
@@ -1,11 +1,17 @@
 # Written by S. Mevawala, modified by D. Gitzel
 
+# Credit to Leart for helping me
+
 import logging
 import socket
 
 import channelsimulator
 import utils
 import sys
+import math
+
+MAX_SEQUENCE = 200
+
 
 class Sender(object):
 
@@ -23,26 +29,122 @@ class Sender(object):
         raise NotImplementedError("The base API class has no implementation. Please override and add your own.")
 
 
-class BogoSender(Sender):
+# class BogoSender(Sender):
+#
+#     def __init__(self):
+#         super(BogoSender, self).__init__()
+#
+#     def send(self, data):
+#         self.logger.info(
+#             "Sending on port: {} and waiting for ACK on port: {}".format(self.outbound_port, self.inbound_port))
+#         while True:
+#             try:
+#                 self.simulator.u_send(data)  # send data
+#                 ack = self.simulator.u_receive()  # receive ACK
+#                 self.logger.info("Got ACK from socket: {}".format(
+#                     ack.decode('ascii')))  # note that ASCII will only decode bytes in the range 0-127
+#                 break
+#             except socket.timeout:
+#                 pass
 
-    def __init__(self):
-        super(BogoSender, self).__init__()
+
+class ReliableSender(Sender):
+
+    global_timeout = 0;
+
+    def __init__(self, data, pkt_size, timeout):
+        super(ReliableSender, self).__init__()
+        self.pkt_size = pkt_size                                                  # Packet size
+        self.pkt_count = int(math.ceil(len(data) // pkt_size)) + 1                # Total number of packets
+        self.timeout = timeout
+        self.global_timeout = timeout
+        self.simulator.sndr_socket.settimeout(self.timeout)                       # Timeout interval
+        self.seq_num = 0                                                          # Initializing sequence number
+        self.pkt_resend = 0                                                       # Number of times it's been resent
+        self.first = True
 
     def send(self, data):
-        self.logger.info("Sending on port: {} and waiting for ACK on port: {}".format(self.outbound_port, self.inbound_port))
-        while True:
+        self.logger.info(
+            "Sending on port: {} and waiting for ACK on port: {}".format(self.outbound_port, self.inbound_port))
+
+        # Split the data into many packets
+        pkt_array = [data[i:i + self.pkt_size] for i in range(0, len(data), self.pkt_size)]
+
+        for i in range(0, self.pkt_count):
             try:
-                self.simulator.u_send(data)  # send data
-                ack = self.simulator.u_receive()  # receive ACK
-                self.logger.info("Got ACK from socket: {}".format(
-                    ack.decode('ascii')))  # note that ASCII will only decode bytes in the range 0-127
-                break
+                # Generates the packet and sends the payload in the correct format
+                pkt = Packet(i, data=pkt_array[i])
+                data_pkt = bytearray([pkt.check_sum, pkt.seq_num]) + pkt_array[i]
+                self.simulator.u_send(data_pkt)
+
+                # Waits for a response from the receiver
+                while True:
+                    ack_pkt = self.simulator.u_receive()
+                    # If the checksum matches the ACK
+                    if ack_pkt[0] == ack_pkt[1]:
+                        # If the sequence number matches the ACK, we don't need to do anything
+
+                        # If the sequence number is less than the ACK, it means that the packet was also accepted and
+                        # the ACK for that sequence number just got lost in the channel so we'll just accept packets
+                        # that are up to 5 ahead
+                        if (self.seq_num + 5) % MAX_SEQUENCE > ack_pkt[1] > self.seq_num:
+                            self.pkt_resend = 0
+                            if self.timeout > self.global_timeout:
+                                self.timeout -= self.global_timeout
+                            self.simulator.sndr_socket.settimeout(self.timeout)
+                            break
+
+                        # If there was some other error, resend
+                        else:
+                            self.simulator.u_send(data_pkt)
+
+                    # If the ACK is corrupted
+                    else:
+                        self._error_resend(data_pkt)
+
+            # If it times out, simply send the data again
             except socket.timeout:
-                pass
+                self._error_resend(data_pkt)
+
+    def _error_resend(self, data_pkt):
+        self.simulator.u_send(data_pkt)
+        self.pkt_resend += 1
+        if self.pkt_resend >= 3:
+            self.timeout *= 2
+            self.simulator.sndr_socket.settimeout(self.timeout)
+            self.pkt_resend = 0
+            # If there's too many timeouts, it quits
+            if self.timeout >= 6:
+                print("RIP")
+                sys.exit()
+
+
+# Packet format: [Checksum, Sequence number, Data]
+class Packet(object):
+
+    def __init__(self, seq_num=0, data=[]):
+        self.seq_num = self._sequence_number(seq_num)
+        self.check_sum = self._checksum(data)
+        self.data = data
+
+    @staticmethod
+    def _checksum(data):
+        check_sum = 0
+        for bit in data:
+            check_sum ^= bit
+        return check_sum
+
+    @staticmethod
+    def _sequence_number(num):
+        return num % MAX_SEQUENCE
 
 
 if __name__ == "__main__":
-    # test out BogoSender
     DATA = bytearray(sys.stdin.read())
-    sndr = BogoSender()
+    # test out BogoSender
+    # sndr = BogoSender()
+    # sndr.send(DATA)
+
+    # ReliableSender(pkt_size, data, timeout)
+    sndr = ReliableSender(DATA, 150, 0.1)
     sndr.send(DATA)
